a22019e4e7e9d7e1b3c6baa866b3323ba655309d
bradley <hull.bradley@ymail.com>
2020-02-09 22:12:46 -0500

    v1.0

--- average_via_input.py
+++ average_via_input.py
@@ -3,32 +3,39 @@ from functools import reduce
 def programy():
     running = True
 
-    print("I output the average of a list that you add files to.")
+    print("I output the average of a list that you add files to. \nType MENU to access the menu. \n")
     listy = []
     
     while running == True:
+        
         def listaverage(givenlist):
             print(sum(listy) / len(listy))
 
         currentnum = input("Please type a number to add to the list: ")
 
-        try:
-            val = int(currentnum)
-        except ValueError:
-            if str(currentnum) == "MENU":
+        
+        if currentnum.isdigit():
+            listy.append(int(currentnum))
+            listaverage(listy)
+        else:
+            if str(currentnum.lower()) == "menu":
                 running = False
             else:
                 print("Not a number!")
-                continue
-   
-        listy.append(int(currentnum))
-        listaverage(listy)
 
-    answer = input("Please type either: EXIT or RESTART")
 
-    if str(answer) == "RESTART":
-        running = True
-    if answer == "EXIT":
-        exit
+active = True
+while active == True:
+    answer = input("Please type either: EXIT or START\n")
+
+    if str(answer.lower()) == "start":
+        print("Starting... \n")
+        programy()
+    if str(answer.lower()) == "exit":
+        print("Now exiting...")
+        active = False
+    else:
+        print("Not valid answer...\n")
+
+
     
-programy()
